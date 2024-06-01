0246f4d7604a230f6e3ced1502cf71f26ad0904b
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2024-06-01 21:32:55 +0600

    banner added

--- src/Layout/Navbar/Navbar.jsx
+++ src/Layout/Navbar/Navbar.jsx
@@ -26,32 +26,32 @@ const Navbar = () => {
     ];
 
     return (
-        <div className='navbar shadow-lg'>
+        <div className='navbar shadow-lg fixed bg-black bg-opacity-50'>
             <div className='navbar-start'>
                 {/* ------------ FOR MOBILE SCREEN -------------- */}
                 <div className='md:hidden'>
                     <MenuBar />
                 </div>
 
-                <Link to="/"><button className='text-3xl font-medium text-teal-500'>Bongo Explorer</button></Link>
+                <Link to="/"><button className='text-3xl font-medium text-[#FFF8DC]'>Bongo Explorer</button></Link>
             </div>
 
 
             {/* ------------------- FOR MEDIUM DEVICE -------------------- */}
             <div className='navbar-center hidden md:flex'>
                 <ul className='menu menu-horizontal '>
-                    <li className='mx-5 text-teal-500'><Link to="/">Home</Link></li>
-                    <li className='mx-5 text-teal-500'><Link to="/package/all">Packages</Link></li>
-                    <li className='mx-5 text-teal-500'><Link to="/story/all">Stories</Link></li>
-                    <li className='mx-5 text-teal-500'><Link to="/dashboard">Dashboard</Link></li>
+                    <li className='mx-5 text-[#FFF8DC]'><Link to="/">Home</Link></li>
+                    <li className='mx-5 text-[#FFF8DC]'><Link to="/package/all">Packages</Link></li>
+                    <li className='mx-5 text-[#FFF8DC]'><Link to="/story/all">Stories</Link></li>
+                    <li className='mx-5 text-[#FFF8DC]'><Link to="/dashboard">Dashboard</Link></li>
                 </ul>
             </div>
 
             <div className='navbar-end'>
                 {/* -------------- LOGIN & REGISTER BUTTON ------------ */}
                 <div className='flex gap-3'>
-                    <Link to="/login"><button className='border px-5 py-3 text-teal-500 border-teal-500 hover:bg-teal-500 hover:text-white'>Login</button></Link>
-                    <Link to="/register"><button className='border px-5 py-3 text-teal-500 border-teal-500 hover:bg-teal-500 hover:text-white'>Register</button></Link>
+                    <Link to="/login"><button className='border px-3 py-1 rounded-md text-[#FFF8DC] border-[#FFF8DC] hover:bg-[#FFF8DC] hover:text-black'>Login</button></Link>
+                    <Link to="/register"><button className='border px-3 py-1 rounded-md text-[#FFF8DC] border-[#FFF8DC] hover:bg-[#FFF8DC] hover:text-black'>Register</button></Link>
                 </div>
 
                 {/* ---------------- PROFILE SECTION -------------- */}
--- src/Layout/Root/Homepage/Homepage.jsx
+++ src/Layout/Root/Homepage/Homepage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from './Banner/Banner';
 
 const Homepage = () => {
     return (
         <div>
-            this is home
+            <Banner/>
         </div>
     );
 };
