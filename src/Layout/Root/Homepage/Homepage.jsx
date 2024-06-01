85f0d76698f23e4dfbc6f1193e40a7bedb80c5e9
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2024-06-01 22:21:02 +0600

    tour and travel section added

--- src/Layout/Root/Homepage/Homepage.jsx
+++ src/Layout/Root/Homepage/Homepage.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import TourTravel from './TourAndTravel/TourTravel';
 
 const Homepage = () => {
     return (
         <div>
             <Banner/>
+
+            <TourTravel/>
         </div>
     );
 };
