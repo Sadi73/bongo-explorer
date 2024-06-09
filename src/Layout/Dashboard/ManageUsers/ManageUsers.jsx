5d49c05730a9b932e064371c2d33088a923b2aed
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2024-06-10 01:15:01 +0600

    manage useres page completed

--- src/Layout/Dashboard/ManageUsers/ManageUsers.jsx
+++ src/Layout/Dashboard/ManageUsers/ManageUsers.jsx
@@ -1,9 +1,58 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const ManageUsers = () => {
+
+    const [allRequest, setAllRequest] = useState([]);
+    const [reload, setReload] = useState(false);
+
+    const handleApprove = (userEmail, requestId) => {
+        axios.put(`https://bongo-traveler.vercel.app/users?email=${userEmail}`)
+            .then(res => {
+                console.log(res?.data)
+                if (res?.data?.modifiedCount) {
+                    axios.delete(`https://bongo-traveler.vercel.app/users/request/${requestId}`)
+                        .then(res => {
+                            if (res?.data?.deletedCount) {
+                                setReload(!reload);
+                            }
+                        })
+                        .catch(error => console.log(error))
+                }
+            })
+            .catch(error => console.log(error))
+    }
+
+    useEffect(() => {
+        axios.get('https://bongo-traveler.vercel.app/users/request')
+            .then(res => {
+                setAllRequest(res?.data)
+            })
+            .catch(error => console.log(error))
+    }, [reload])
+
     return (
-        <div>
-            ManageUsers
+        <div className='mt-10'>
+            {allRequest.map(request =>
+                <div key={request?._id}
+                    className='flex justify-between items-center border rounded-lg p-5 shadow-lg'
+                >
+                    <div>
+                        <p>User Email</p>
+                        <p>{request?.userEmail}</p>
+                    </div>
+                    <div>
+                        <p>Requested Role</p>
+                        <p>{request?.requestedRole}</p>
+                    </div>
+                    <div>
+                        <button
+                            className='bg-green-500 text-white px-5 py-3 rounded-md'
+                            onClick={() => handleApprove(request?.userEmail, request?._id)}
+                        >Approve</button>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
