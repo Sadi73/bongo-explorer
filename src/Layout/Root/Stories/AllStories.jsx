7602bde3a8939d087935e767cf27b73ced4eba16
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2024-06-10 10:22:36 +0600

    stories page completed with api integration

--- src/Layout/Root/Stories/AllStories.jsx
+++ src/Layout/Root/Stories/AllStories.jsx
@@ -1,23 +1,36 @@
-import React from 'react';
-import storyImg from '../../../assets/storyImage1.jpg'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import EmptyPage from '../../Dashboard/EmptyPage/EmptyPage';
 
 const AllStories = () => {
+    const [allStories, setAllStories] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/stories')
+            .then(res => setAllStories(res?.data))
+            .catch(error => console.log(error))
+    }, []);
+
+    console.log(allStories)
+
     return (
-        <div className='pt-20 flex text-center'>
-            <div className='bg-[#f9f9f9] leading-8'>
-                <p >Australia</p>
-                <h1 className='text-2xl'>Want to see the countryside</h1>
-                <p >Maria Nur</p>
+        <div className='pt-28 px-10'>
+            {allStories.length > 0 ?
+                allStories.map(story =>
+                    <div key={story?._id} className='flex text-center mb-8 border shadow-xl rounded-xl justify-between'>
+                        <div className='leading-8'>
+                            <p >Australia</p>
+                            <h1 className='text-2xl'>{story?.name}</h1>
+                            <p >Maria Nur</p>
+                            <p >{story?.tour_story}</p>
+                            <p>Keep reading...</p>
+                        </div>
 
-                <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia numquam quos dicta maxime odit dolore quis, necessitatibus nostrum. Aliquid possimus quos ad, modi deleniti at voluptatum placeat ab! Dignissimos, rerum!</p>
-
-                <p>Keep reading...</p>
-            </div>
-
-            <div>
-                <img src={storyImg} alt="" />
-            </div>
-
+                        <div className='w-1/3'>
+                            <img src={story?.tour_image} alt="" className='w-full rounded-r-xl' />
+                        </div>
+                    </div>) : <EmptyPage />
+            }
         </div>
     );
 };
