1009a7c8d7f357b955857d2a1cdce7167fbc0b69
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2024-10-05 14:00:27 +0600

    banner section updater

--- src/Layout/Root/Homepage/Banner/Banner.jsx
+++ src/Layout/Root/Homepage/Banner/Banner.jsx
@@ -1,12 +1,44 @@
 import React from 'react';
-import img from '../../../../assets/1.jpg';
+import { Carousel } from 'antd';
+import img1 from '../../../../assets/BannerImages/1.jpg';
+import img3 from '../../../../assets/BannerImages/3.jpg';
+import img5 from '../../../../assets/BannerImages/5.jpg';
+import img8 from '../../../../assets/BannerImages/8.jpeg';
 
-const Banner = () => {
-    return (
-        <div>
-            <img src={img} alt=""  className='w-full h-screen'/>
-        </div>
-    );
+const contentStyle = {
+    height: '160px',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
 };
-
+const Banner = () => (
+    <Carousel autoplay>
+        
+        <div className="relative w-full h-[700px]">
+            <img src={img1} className="w-full h-full object-cover" alt="gradient overlay image" />
+            <div className="absolute inset-0 bg-gradient-to-r from-black/50 to-transparent">
+                <span className="absolute bottom-10 right-96 text-white/60 text-6xl font-bold">Beautiful Rural Bangladesh</span>
+            </div>
+        </div>
+        <div className="relative w-full h-[700px]">
+            <img src={img3} className="w-full h-full object-cover" alt="gradient overlay image" />
+            <div className="absolute inset-0 bg-gradient-to-r from-black/50 to-transparent">
+                <span className="absolute bottom-5 left-5 text-white text-2xl font-bold">Ahsan Monjil</span>
+            </div>
+        </div>
+        <div className="relative w-full h-[700px]">
+            <img src={img5} className="w-full h-full object-cover" alt="gradient overlay image" />
+            <div className="absolute inset-0 bg-gradient-to-t from-black/50 to-transparent">
+                <span className="absolute bottom-5 right-5 text-white text-2xl font-bold">Sajek Valley</span>
+            </div>
+        </div>
+        <div className="relative w-full h-[700px]">
+            <img src={img8} className="w-full h-full object-cover" alt="gradient overlay image" />
+            <div className="absolute inset-0 bg-gradient-to-r from-black/50 to-transparent">
+                <span className="absolute bottom-5 left-5 text-white text-2xl font-bold">Saint Martin Island</span>
+            </div>
+        </div>
+    </Carousel>
+);
 export default Banner;
